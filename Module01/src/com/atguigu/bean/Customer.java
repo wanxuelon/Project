950a7cb03a9361ab3df614bd9a8ee0e7b1ea0012
wan_pro <wanxuelon_pro@163.com>
2020-02-29 16:58:59 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,9 @@
+package com.atguigu.bean;
+
+public class Customer {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("HelloWord!!!");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import sun.text.CodePointIterator;
+
+import java.util.ArrayList;
+import java.util.prefs.Preferences;
+
+/**
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code hereSystem.out.println("HelloWord");
+
+        
+    }
+
+    public  void  module(){
+
+        ArrayList list  = new ArrayList();
+
+        private int b;
+
+        System.out.println("  ");
+        }
+    }
+
+
+}
+
